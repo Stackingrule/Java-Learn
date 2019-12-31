46de03e35fac56ba81df36b16ab283d733850347
Stackingrule <messageswitching@gmail.com>
2019-12-31 18:23:49 +0800

    complete Spring MVC

--- SpringBoot/SprintBoot-Nowcoder/community/src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ SpringBoot/SprintBoot-Nowcoder/community/src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -3,13 +3,15 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.service.AlphaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Enumeration;
+import java.util.*;
 
 /*
 * Created by Stackingrule on 2019/12/30
@@ -94,6 +96,59 @@ public class AlphaController {
 
 
     // 响应html数据
+    @RequestMapping(path = "/teacher", method = RequestMethod.GET)
+    public ModelAndView getTeacher() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name", "张三");
+        modelAndView.addObject("age", 30);
+        modelAndView.setViewName("/demo/view");
+        return modelAndView;
+    }
 
+
+    @RequestMapping(path = "/school", method = RequestMethod.GET)
+    public String getSchool(Model model) {
+        model.addAttribute("name", "北京大学");
+        model.addAttribute("age", "120");
+        return "/demo/view";
+    }
+
+
+    // 响应json数据（异步请求）
+    @RequestMapping(path = "/emp", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> getEmp() {
+        Map<String, Object> emp = new HashMap<>();
+        emp.put("name", "张三");
+        emp.put("age", 23);
+        emp.put("salary", 8000.00);
+
+        return emp;
+    }
+
+    
+    @RequestMapping(path = "/emps", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Map<String, Object>> getEmps() {
+        List<Map<String, Object>> list = new ArrayList<>();
+        Map<String, Object> emp = new HashMap<>();
+
+        emp.put("name", "张三");
+        emp.put("age", 23);
+        emp.put("salary", 8000.00);
+        list.add(emp);
+
+        emp.put("name", "李四");
+        emp.put("age", 20);
+        emp.put("salary", 10000.00);
+        list.add(emp);
+
+        emp.put("name", "王五");
+        emp.put("age", 26);
+        emp.put("salary", 9000.00);
+        list.add(emp);
+
+        return list;
+    }
 
 }
