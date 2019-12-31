fed929dc8ee62c7ad8309c970a731dbd98a2f3fa
Stackingrule <messageswitching@gmail.com>
2019-12-31 09:45:59 +0800

    learn Spring MVC

--- SpringBoot/SprintBoot-Nowcoder/community/src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ SpringBoot/SprintBoot-Nowcoder/community/src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -3,8 +3,13 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.service.AlphaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
 
 /*
 * Created by Stackingrule on 2019/12/30
@@ -27,4 +32,68 @@ public class AlphaController {
     public String getData() {
         return alphaService.find();
     }
+
+
+    @RequestMapping("/http")
+    public void http(HttpServletRequest request, HttpServletResponse response) {
+        // 获取请求对象
+        System.out.println(request.getMethod());
+        System.out.println(request.getServletPath());
+        Enumeration<String> enumeration = request.getHeaderNames();
+        while (enumeration.hasMoreElements()) {
+            String name = enumeration.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name + ": " + value);
+        }
+        System.out.println(request.getParameter("code"));
+
+        // 返回响应数据
+        response.setContentType("text/html; charset=utf-8");
+        try(
+                PrintWriter writer = response.getWriter();
+                ) {
+            writer.write("<h1>Nowcoder</h1>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // GET请求
+
+    // /students?current=1 & limit=20
+    @RequestMapping(path="/students", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudents(
+            @RequestParam(name = "current", required = false, defaultValue = "1") int current,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) {
+        System.out.println(current);
+        System.out.println(limit);
+        return "some students!";
+    }
+
+    // /student/123
+    @RequestMapping(path = "/student/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudent(@PathVariable("id") int id) {
+        System.out.println(id);
+        return "a student!";
+    }
+
+
+
+    // POST 请求
+    @RequestMapping(path = "/student", method = RequestMethod.POST)
+    @ResponseBody
+    public String saveStudent(String name, int age) {
+        System.out.println(name);
+        System.out.println(age);
+        return "success!";
+    }
+
+
+
+    // 响应html数据
+
+
 }
