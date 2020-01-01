bb283d23d9105367eddbebc95718b10c2fe11da3
Stackingrule <messageswitching@gmail.com>
2020-01-01 16:27:12 +0800

    add DiscussPost

---
+++ SpringBoot/SprintBoot-Nowcoder/community/src/main/java/com/nowcoder/community/entity/DiscussPost.java
@@ -0,0 +1,100 @@
+package com.nowcoder.community.entity;
+
+import java.util.Date;
+
+public class DiscussPost {
+
+    private int id;
+
+    private int userId;
+
+    private String title;
+
+    private String content;
+
+    private int type;
+
+    private Date createDate;
+
+    private int commentCount;
+
+    private double score;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(int commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "DiscussPost{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", type=" + type +
+                ", createDate=" + createDate +
+                ", commentCount=" + commentCount +
+                ", score=" + score +
+                '}';
+    }
+}
