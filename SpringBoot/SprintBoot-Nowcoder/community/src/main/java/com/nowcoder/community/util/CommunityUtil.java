53c83cda859faa4a670a6969ce85c1830656bbf2
Stackingrule <messageswitching@gmail.com>
2020-01-04 21:52:59 +0800

    add register

--- SpringBoot/SprintBoot-Nowcoder/community/src/main/java/com/nowcoder/community/util/CommunityUtil.java
+++ SpringBoot/SprintBoot-Nowcoder/community/src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 public class CommunityUtil {
 
+    // 生成随机字符串
     public static String generateUUID() {
         return UUID.randomUUID().toString().replace("-", "");
     }
